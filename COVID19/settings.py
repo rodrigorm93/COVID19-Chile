8e3d26ed5abca899c558f920c7191de4b2f6b5f1
RodrigoRM <33364100+rodrigorm93@users.noreply.github.com>
2020-05-27 21:54:00 -0400

    Update settings.py

--- COVID19/settings.py
+++ COVID19/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd%tr5zum!bn%h9^jed8^jo*t4ydvjwxvi*#z^qs^c4fyt-b58t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
